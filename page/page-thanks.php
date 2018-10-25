1090bb3d6816f5e18cfb4b52c98a9aca108044a5
ifreeworklancer <ifreeworklancer@gmail.com>
2018-10-25 12:13:40 +0300

    page-thanks_first

--- page/page-thanks.php
+++ page/page-thanks.php
@@ -1,16 +1,18 @@
 <?php
 
-require_once('../views/base/header.php');
+require_once('../views/base/header-secondary.php');
+
+?>
 
     <section id="thanks">
         <div class="container h-100 d-flex justify-content-center align-items-center">
-            <div class="row">
-                <div class="col-sm-5 col-lg-4">
+            <div class="row w-100">
+                <div class="col-sm-6 col-lg-4 mx-auto">
                     <div class="thanks-item text-center text-white">
-                        <h1 class="thanks-item__title">
-                            Спасибо
+                        <h1 class="thanks-item__title text-uppercase">
+                            Спасибо!
                         </h1>
-                        <p class="thanks-item__descr">
+                        <p class="thanks-item__descr my-4">
                             Наш менеджер с Вами свяжется...
                             Уже звонит...
                         </p>
@@ -23,4 +25,6 @@ require_once('../views/base/header.php');
         </div>
     </section>
 
-require_once('../views/base/footer.php');
+<?php
+
+require_once('../views/base/footer-secondary.php');
