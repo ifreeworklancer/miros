80036981dbf24f07216fc8ff85dab5f163d401ba
Oleh Talanoff <talanov.o@gmail.com>
2018-10-30 17:10:32 +0200

    #

--- thanks.php
+++ thanks.php
@@ -1,21 +1,17 @@
 <?php require_once(__DIR__ . '/views/base/header-secondary.php'); ?>
 
     <section id="thanks">
-        <div class="container h-100 d-flex justify-content-center align-items-center">
-            <div class="row">
-                <div class="col-sm-5 col-lg-4">
-                    <div class="thanks-item text-center text-white">
-                        <h1 class="thanks-item__title">
-                            Спасибо!
-                        </h1>
-                        <p class="thanks-item__descr">
-                            Наш менеджер с Вами свяжется...
-                        </p>
-                        <a href="/" class="btn btn-decor">
-                            На главную
-                        </a>
-                    </div>
-                </div>
+        <div class="container h-100 d-flex justify-content-center flex-column">
+            <div class="text-center text-white">
+                <h1 class="thanks-item__title">
+                    Спасибо!
+                </h1>
+                <p>
+                    Наш менеджер с Вами свяжется...
+                </p>
+                <a href="/" class="btn btn-decor">
+                    На главную
+                </a>
             </div>
         </div>
     </section>
