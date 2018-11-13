6aaff820de530d85e1219a1cc00c03916e71939e
Oleh Talanoff <talanov.o@gmail.com>
2018-11-13 14:06:03 +0200

    #add novaposhta

--- novaposhta.php
+++ novaposhta.php
@@ -2,7 +2,7 @@
 
 require_once (__DIR__ . '/vendor/autoload.php');
 
-use App\NovaPoshta;
+use \app\NovaPoshta;
 
 if (isset($_POST['city'])) {
 	echo (new NovaPoshta())->getWarehouses($_POST['city']);
